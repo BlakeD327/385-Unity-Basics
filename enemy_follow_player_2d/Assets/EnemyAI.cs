c2841dea58ee050118f3d2d3beb9e1d438b2e1a5
BlakeD327 <47874373+BlakeD327@users.noreply.github.com>
2023-04-02 22:12:09 -0700

    added and changed enemy_follow_player_2d

---
+++ enemy_follow_player_2d/Assets/EnemyAI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public GameObject player;
+    public SpriteRenderer destination;
+    private float speed = 10f;
+    
+    private void Update()
+    {
+        if (Vector2.Distance(transform.position, player.transform.position) > 5f) {
+
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, 
+                player.transform.position, step);
+        }
+
+        if (Vector2.Distance(transform.position, player.transform.position) > 0.7f) {
+
+            float step = (speed/2) * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, 
+                player.transform.position, step);
+        }
+    }    
+}
