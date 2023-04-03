e9f55b8dca6cb4d991f70ad6591656522c631a62
BlakeD327 <47874373+BlakeD327@users.noreply.github.com>
2023-04-02 20:13:45 -0700

    Modified enemy_follow_player_2d

--- enemy_follow_player_2d/Assets/EnemyAI.cs
+++ enemy_follow_player_2d/Assets/EnemyAI.cs
@@ -3,14 +3,23 @@
 public class EnemyAI : MonoBehaviour
 {
     public GameObject player;
-    private float speed = 5f;
-
+    public SpriteRenderer destination;
+    private float speed = 10f;
+    
     private void Update()
     {
-        if (Vector2.Distance(transform.position, player.transform.position) > 0.7f)
-        {
+        if (Vector2.Distance(transform.position, player.transform.position) > 5f) {
+
             float step = speed * Time.deltaTime;
-            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, step);
+            transform.position = Vector2.MoveTowards(transform.position, 
+                player.transform.position, step);
         }
-    }
+
+        if (Vector2.Distance(transform.position, player.transform.position) > 0.7f) {
+
+            float step = (speed/2) * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, 
+                player.transform.position, step);
+        }
+    }    
 }
